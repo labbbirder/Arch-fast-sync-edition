643ef6813cc055db01bd32677824a9702459f799
labbbirder <502100554@qq.com>
2024-05-27 19:26:00 +0800

    fix: sync world error

--- src/Arch/Core/World.Serialization.cs
+++ src/Arch/Core/World.Serialization.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Arch.Core.Utils;
 using Arch.LowLevel;
+using Arch.LowLevel.Jagged;
 namespace Arch.Core;
 
+public interface IClonableComponent : ICloneable
+{
+    //public object Clone(Dictionary<object,object> managedObjectTable)
+    //{
+    //    ICloneable prop = new object() as ICloneable;
+    //    ICloneable newProp = null;
+    //    if (managedObjectTable.TryGetValue(prop,out var obj))
+    //    {
+    //        newProp = obj as ICloneable;
+    //    }
+    //    else
+    //    {
+    //        managedObjectTable[prop] = prop.Clone();
+    //    }
+
+    //}
+}
 public partial class World
 {
     private World _syncingWorld = null;
+    private Dictionary<Archetype, Archetype> _archetypeLut = new();
     public void SetSyncingSourceWorld(World another)
     {
         _syncingWorld = another;
+        _archetypeLut.Clear();
     }
 
     public void Sync()
@@ -27,31 +48,113 @@ public partial class World
 
         var dstArchetypes = Archetypes;
         var srcArchetypes = _syncingWorld.Archetypes;
-        for(var i = srcArchetypes.Count - 1; i >= 0; i--)
+        for (var i = 0; i < srcArchetypes.Count; i++)
         {
             var srcArche = srcArchetypes[i];
             var dstArche = GetOrCreate(srcArche.Types);
+            _archetypeLut[srcArche] = dstArche;
+
             dstArche.Clear();
-            dstArche.EnsureEntityCapacity(srcArche.ChunkCapacity);
+            dstArche.EnsureEntityCapacity(srcArche.EntityCapacity);
+
+            dstArche.ChunkCount = srcArche.ChunkCount;
+            dstArche.EntityCount = srcArche.EntityCount;
+
+            // sync chunks in a archetype
             var j = 0;
             for (; j < srcArche.ChunkCount; j++)
             {
-                var srcChunk = srcArche.GetChunk(i);
-                var dstChunk = dstArche.GetChunk(i);
-                Array.ConstrainedCopy(srcChunk.Entities, 0, dstChunk.Entities, 0, srcChunk.Size);
-                for (var k = 0; k < srcArche.Types.Length; k++)
-                {
-                    Array.ConstrainedCopy(srcChunk.Components[k], 0, dstChunk.Components[k], 0, srcChunk.Size);
-                }
-                dstChunk.Size = srcChunk.Size;
+                ref var srcChunk = ref srcArche.GetChunk(j);
+                ref var dstChunk = ref dstArche.GetChunk(j);
+                SyncChunk(ref srcChunk, ref dstChunk, srcArche.Types);
             }
-            dstArche.ChunkCount = srcArche.ChunkCount;
             //for (; j < dstArche.ChunkCount; j++)
             //{
-                // Do some clean work here...
+            // Do some clean work here...
             //}
         }
 
+        var dstEntityInfo = EntityInfo;
+        var srcEntityInfo = _syncingWorld.EntityInfo;
+        SyncJaggedArray(srcEntityInfo.Versions, dstEntityInfo.Versions);
+        SyncEntitySlots(srcEntityInfo.EntitySlots, dstEntityInfo.EntitySlots, _archetypeLut);
+        Size = _syncingWorld.Size;
+        QueryCache.Clear();
+    }
 
+
+    private unsafe void SyncJaggedArray<T>(JaggedArray<T> srcArr, JaggedArray<T> dstArr) where T : unmanaged
+    {
+        dstArr.EnsureCapacity(srcArr.Capacity);
+        int i = 0;
+        for (; i < srcArr.Buckets; i++)
+        {
+            ref var srcBucket = ref srcArr.GetBucket(i);
+            ref var dstBucket = ref dstArr.GetBucket(i);
+            fixed (T* src = &srcBucket[0], dst = &dstBucket[0])
+            {
+                var bytesCount = srcBucket.Count * sizeof(T);
+                System.Buffer.MemoryCopy(src, dst, bytesCount, bytesCount);
+            }
+            dstBucket.Count = srcBucket.Count;
+        }
+        for (; i < dstArr.Buckets; i++)
+        {
+            var dstBucket = dstArr.GetBucket(i);
+            dstBucket.Count = 0;
+        }
+        //dstArr.TrimExcess(); // TODO: This may be harmful to performance
+    }
+
+    private void SyncEntitySlots(JaggedArray<EntitySlot> srcArr, JaggedArray<EntitySlot> dstArr, Dictionary<Archetype,Archetype> lut)
+    {
+        dstArr.EnsureCapacity(srcArr.Capacity);
+        int i = 0;
+        for (; i < srcArr.Buckets; i++)
+        {
+            ref var srcBucket = ref srcArr.GetBucket(i);
+            ref var dstBucket = ref dstArr.GetBucket(i);
+            for(int j=0; j< srcBucket.Count; j++)
+            {
+                dstBucket[j] = srcBucket[j];
+                dstBucket[j].Archetype = lut[srcBucket[j].Archetype];
+            }
+
+            dstBucket.Count = srcBucket.Count;
+        }
+
+        for (; i < dstArr.Buckets; i++)
+        {
+            var dstBucket = dstArr.GetBucket(i);
+            dstBucket.Count = 0;
+        }
+
+        //dstArr.TrimExcess(); // TODO: This may be harmful to performance
+    }
+
+    public void SyncChunk(ref Chunk srcChunk, ref Chunk dstChunk, ComponentType[] types)
+    {
+        Array.ConstrainedCopy(srcChunk.Entities, 0, dstChunk.Entities, 0, srcChunk.Size);
+        for (var k = 0; k < types.Length; k++)
+        {
+            var srcType = types[k].Type;
+            if (!typeof(ICloneable).IsAssignableFrom(srcType))
+            {
+                Array.ConstrainedCopy(srcChunk.Components[k], 0, dstChunk.Components[k], 0, srcChunk.Size);
+            }
+            else
+            {
+                CloneCloneableArray(srcChunk.Components[k], dstChunk.Components[k], srcChunk.Size);
+            }
+        }
+        dstChunk.Size = srcChunk.Size;
+    }
+
+    private void CloneCloneableArray(Array src,Array dst,int length)
+    {
+        for (var i = 0;i < length; i++)
+        {
+            dst.SetValue((src.GetValue(i) as ICloneable)?.Clone(), i); 
+        }
     }
 }
